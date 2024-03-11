33f8880f60d9b37f96d27a513173abdc96e0979c
Hosik <orrorin@naver.com>
2024-03-11 21:30:11 +0900

    Time: 3 ms (68.69%), Space: 44.8 MB (61.91%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> positive = new HashMap<>(); // hashMap의 최대 크기 2의 30승 (1억개가 조금 넘음)
+        HashMap<Integer, Integer> negative = new HashMap<>();
+
+        int n, t;
+        int[] answer = new int[2];
+        for(int i = 0; i < nums.length; i++) {
+            n = nums[i];
+            t = target - n;
+            if(positive.containsKey(t)) {
+                answer[0] = positive.get(t);
+                answer[1] = i;
+                break;
+            }
+
+            if(negative.containsKey(t)) {
+                answer[0] = negative.get(t);
+                answer[1] = i;
+                break;
+            }
+
+            if (n >= 0) positive.put(n, i);
+            else negative.put(n, i);
+        }
+
+        return answer;
+    }
+}
