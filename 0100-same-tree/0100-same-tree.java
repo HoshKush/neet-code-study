1c71d3727c7f0316e8caae869dd3672698aa1daf
Hosik <orrorin@naver.com>
2024-04-08 21:43:15 +0900

    Time: 0 ms (100%), Space: 40.9 MB (59.51%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -18,9 +18,6 @@ class Solution {
         if(p == null && q == null) return true;
         if(p == null || q == null) return false;
         if(p.val != q.val) return false;
-        if(!isSameTree(p.left, q.left)) {
-            return false;
-        }
-        return isSameTree(p.right, q.right);
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
